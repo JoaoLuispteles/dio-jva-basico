6b572748836a73e7b141449cc5e15db86dd640ea
João Luís <joaoluispedrosateles145@gmail.com>
2024-06-10 18:05:16 -0300

    Simulando uma SmartTV

--- SmartTV/src/SmartTV.java
+++ SmartTV/src/SmartTV.java
@@ -5,9 +5,12 @@ public class SmartTV {
 
     public void aumentarVolume(){
         volume++;
+        System.out.println("Aumentando o volume para: " + volume);
     }
     public void diminuirVolume(){
         volume--;
+        System.out.println("Diminuindo o volume para: " + volume);
+
     }
     public void ligar(){
         ligada = true;
