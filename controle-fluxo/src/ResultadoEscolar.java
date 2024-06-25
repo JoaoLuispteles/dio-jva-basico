6b28d9da8fab717289a4212101ae26cc4748856a
João Luís <joaoluispedrosateles145@gmail.com>
2024-06-25 16:31:43 -0300

    condicionais

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -6,7 +6,7 @@ public class ResultadoEscolar {
         if (nota >= 7) {
             System.out.println("Aprovado!");
         }
-        else if (nota > 5 && nota < 7) {
+        else if (nota >= 5 && nota < 7) {
             System.out.println("Recuperação!");
         }
         else {
